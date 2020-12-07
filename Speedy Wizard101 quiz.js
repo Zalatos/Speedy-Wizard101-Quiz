1d05e3f3eef78c9bb21d4e33a849a8077d64da8e
Ozzy <Dev@ecasa.co.za>
2020-12-07 10:32:20 +0200

    git init

---
+++ Speedy Wizard101 quiz.js
@@ -0,0 +1,33 @@
+/* global $ */
+// ==UserScript==
+// @name         Speedy Wizard101 Quiz
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Revise wizard101. quiz to make it faster to do the quiz
+// @author       Zalatos
+// @match        https://www.wizard101.com/quiz/trivia/game*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.debug("on wizard101 quiz");
+    var ZalatosWizard101QuizEnhancer = () => {
+        //find answer boxes
+        let answers = document.getElementsByClassName("answer");
+        for (var i in answers ){
+            if(typeof answers[i].classList !== "undefined"){
+                console.debug("attempting to remove fadein from answer: " + answers[i].innerText);
+                answers[i].classList.remove("fadeIn");
+                console.debug(answers[i].classList);
+            }
+        }
+    }
+    if (typeof $ === "undefined"){
+        setTimeout(ZalatosWizard101QuizEnhancer,400);
+    }
+    else {
+        $(ZalatosWizard101QuizEnhancer);
+        $(()=>{console.debug("executed fadeIn removal script with jquery")});
+    }
+})();
