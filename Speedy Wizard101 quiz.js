a5a567679bb16d267923720cf42356b2097b8010
Ozzy <Dev@ecasa.co.za>
2020-12-07 10:01:41 +0200

    make whole answer clickable to select insead of jsut checkbox

--- Speedy Wizard101 quiz.js
+++ Speedy Wizard101 quiz.js
@@ -1,8 +1,8 @@
-/* global jQuery */
+/* global jQuery,selectQuizAnswer */
 // ==UserScript==
 // @name         Speedy Wizard101 Quiz
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Revise wizard101. quiz to make it faster to do the quiz
 // @author       Zalatos
 // @match        https://www.wizard101.com/quiz/trivia/game*
@@ -17,6 +17,12 @@
         jQuery(".answer").css("visibility", "visible");
         //show next button immediately
         jQuery("button").css("visibility", "visible");
+
+        //make checkbox click area apply to entire box not just check box
+        jQuery(".answer").click(()=>{
+            //use built in function to select item
+            selectQuizAnswer(jQuery(this).find('.largecheckbox').get(0));
+        })
     }
         jQuery(ZalatosWizard101QuizEnhancer);
         jQuery(()=>{console.debug("Speedy Wizard101 Quiz")});
@@ -31,4 +37,23 @@
     styleEle.innerHTML = revisedFadeStyle;
     htmlHead.appendChild(styleEle);
 
+    //override built in function for selecting quiz answer
+    selectQuizAnswer  = function(j) {
+        if(localStorage.getItem("selectionInProgress") === true) { //Quiz enhancement code
+            localStorage.setItem("selectionInProgress", true); //Quiz enhancement code
+            var k = a.getElementsByClassName("answerBox");
+            for (var h = 0; h < k.length; h++) {
+                if (k[h].children[0] === j) {
+                    k[h].children[0].className = "largecheckboxselected";
+                    k[h].children[1].checked = "checked"
+                } else {
+                    k[h].children[0].className = "largecheckbox";
+                    k[h].children[1].checked = ""
+                }
+            }
+            //change the value after this cpu clock cycle.
+            //this is to overcome the function running twice when using the actual checkbox for selection purposes
+            setTimeout(()=>{localStorage.setItem("selectionInProgress", false)},1); //Quiz enhancement code
+        }
+    };
 })();
