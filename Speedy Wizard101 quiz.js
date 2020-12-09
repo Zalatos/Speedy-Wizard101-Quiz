4703ff46c49ee55998bd6d2f96cb0888212138cb
Ozzy <Dev@ecasa.co.za>
2020-12-09 12:52:58 +0200

    -switch to event source to reference object -rename function reference to make it unminised for readability -change timings on double running of function to 10ms from 1ms

--- Speedy Wizard101 quiz.js
+++ Speedy Wizard101 quiz.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Speedy Wizard101 Quiz
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Revise wizard101. quiz to make it faster to do the quiz
 // @author       Zalatos
 // @match        https://www.wizard101.com/quiz/trivia/game*
@@ -19,10 +19,12 @@
         jQuery("button").css("visibility", "visible");
 
         //make checkbox click area apply to entire box not just check box
-        jQuery(".answer").click(()=>{
+        jQuery(".answer").click((evt)=>{
+            //find checkbox link
+            let link = jQuery(evt.currentTarget).find('.largecheckbox').get(0) || jQuery(evt.currentTarget).find('.largecheckboxselected').get(0);
             //use built in function to select item
-            selectQuizAnswer(jQuery(this).find('.largecheckbox').get(0));
-        })
+            selectQuizAnswer(link);
+        });
     }
         jQuery(ZalatosWizard101QuizEnhancer);
         jQuery(()=>{console.debug("Speedy Wizard101 Quiz")});
@@ -38,13 +40,13 @@
     htmlHead.appendChild(styleEle);
 
     //override built in function for selecting quiz answer
-    selectQuizAnswer = function(j) {
+    selectQuizAnswer = function(selectedCheckbox) {
+        console.debug(localStorage.getItem("selectionInProgress"));
         if(localStorage.getItem("selectionInProgress") === "false") { //Quiz enhancement code
             localStorage.setItem("selectionInProgress", true); //Quiz enhancement code
-            //replaced variable a to the quiz container since a is no longer accessible from the orignal script context
-            var k = jQuery("#quizContainer").get(0).getElementsByClassName("answerBox");
+            var k = document.querySelectorAll(".answerBox");
             for (var h = 0; h < k.length; h++) {
-                if (k[h].children[0] === j) {
+                if (k[h].children[0] === selectedCheckbox) {
                     k[h].children[0].className = "largecheckboxselected";
                     k[h].children[1].checked = "checked"
                 } else {
@@ -52,9 +54,12 @@
                     k[h].children[1].checked = ""
                 }
             }
+            
             //change the value after this cpu clock cycle.
             //this is to overcome the function running twice when using the actual checkbox for selection purposes
-            setTimeout(()=>{localStorage.setItem("selectionInProgress", false)},1); //Quiz enhancement code
+            setTimeout(()=>{localStorage.setItem("selectionInProgress", false)},10); //Quiz enhancement code
         }
     };
+    //start with it as false. seems to be starting as true.
+    localStorage.setItem("selectionInProgress", false)
 })();
